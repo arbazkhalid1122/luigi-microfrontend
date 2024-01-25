f6363eb64dbda629497fdf77b3e9af10a3d12787
Arbaz khalid <khalidarbaz27@gmail.com>
2024-01-25 19:31:09 +0500

    fix

---
+++ container/vuejs/src/views/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Vue js</h1>
+  <div v-if="total > 0">total count is : {{total}}</div>
+</template>
+
+<script>
+import LuigiClient from "@luigi-project/client";
+
+
+export default {
+  name: "home",
+  data: () => ({
+    total: LuigiClient.getNodeParams().total,
+  }), 
+  
+  created() {
+    this.luigiClient = LuigiClient; 
+
+    this.luigiClient.addContextUpdateListener((updatedContext) => {
+      this.total =this.luigiClient.getNodeParams().total;
+    });
+
+    this.luigiClient.addInitListener((initialContext) => {
+      this.total =this.luigiClient.getNodeParams().total;
+    });
+  },
+};
+
+</script>
